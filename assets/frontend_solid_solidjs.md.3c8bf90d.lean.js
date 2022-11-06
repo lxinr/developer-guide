fb7c68089d168f3f85f1e467729c9d7acb7eeea0
beierzhijin <beierzhijin@users.noreply.github.com>
2022-11-06 15:48:39 +0000

    deploy: 72c03c7984db8a43fa2fe99fed0d3d654d1d016e

---
+++ assets/frontend_solid_solidjs.md.3c8bf90d.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as s}from"./app.75b212e0.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"frontend/solid/solidjs.md","lastUpdated":1667749611000}'),o={name:"frontend/solid/solidjs.md"};function a(r,n,d,c,i,l){return t(),s("div")}const f=e(o,[["render",a]]);export{_ as __pageData,f as default};
